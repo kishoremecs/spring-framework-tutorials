b2c2522be99bdfcfaf4dd232760f380a181b5511
Kishore Kumar Rachuti <kmr.kishore@gmail.com>
2024-06-08 13:26:04 +0530

    Caching: Added hazelcast.

--- spring-cache-example/src/main/java/com/caching/practice/config/SimpleCacheCustomizer.java
+++ spring-cache-example/src/main/java/com/caching/practice/config/SimpleCacheCustomizer.java
@@ -2,9 +2,11 @@ package com.caching.practice.config;
 
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Profile;
 
 import static java.util.Arrays.asList;
 
+@Profile("simple")
 public class SimpleCacheCustomizer implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
     @Override
     public void customize(ConcurrentMapCacheManager cacheManager) {
