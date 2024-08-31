d6a9aea7543234396f7b94dedb41a5faca9c621a
Kishore Kumar Rachuti <kmr.kishore@gmail.com>
2024-09-01 00:38:53 +0530

    Enhanced the test methods.

--- spring-boot-hello/src/test/java/com/practice/controllers/HelloRestControllerTest.java
+++ spring-boot-hello/src/test/java/com/practice/controllers/HelloRestControllerTest.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +24,13 @@ public class HelloRestControllerTest {
     public void testHello(@Autowired TestRestTemplate webTestClient) {
         var greeting = webTestClient.getForObject("/greet?name={name}", Greeting.class, "Kishore");
         assertEquals("Hello, Kishore!", greeting.message());
+    }
+
+    @Test
+    public void greetWithName(@Autowired TestRestTemplate webTestClient) {
+        var greeting = webTestClient.getForEntity("/greet?name={name}", Greeting.class, "Kishore");
+        assertEquals("Hello, Kishore!", Objects.requireNonNull(greeting.getBody()).message());
+        assertTrue(greeting.getStatusCode().is2xxSuccessful());
+        assertEquals(MediaType.APPLICATION_JSON,greeting.getHeaders().getContentType());
     }
 }
