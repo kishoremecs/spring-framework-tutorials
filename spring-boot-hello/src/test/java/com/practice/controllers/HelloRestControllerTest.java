314a353c8438213d198689e53bcd37765fea2896
Kishore Kumar Rachuti <kmr.kishore@gmail.com>
2024-09-01 00:27:15 +0530

    Removed Unused Imports.

--- spring-boot-hello/src/test/java/com/practice/controllers/HelloRestControllerTest.java
+++ spring-boot-hello/src/test/java/com/practice/controllers/HelloRestControllerTest.java
@@ -1,2 +1,17 @@
-package com.practice.controllers;public class HelloRestControllerTest {
+package com.practice.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloRestControllerTest {
+
+    @Test
+    public void testHello(@Autowired TestRestTemplate webTestClient) {
+        String body = webTestClient.getForObject("/greet?name={name}", String.class, "Kishore");
+        assertEquals("{\"message\":\"Hello, Kishore!\"}", body);
+    }
 }
