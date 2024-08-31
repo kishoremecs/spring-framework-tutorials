ed8f88a8e7f2ca839732c9f1c27a10dca52756f3
Kishore Kumar Rachuti <kmr.kishore@gmail.com>
2024-09-01 00:31:25 +0530

    Enhanced the test methods.

--- spring-boot-hello/src/test/java/com/practice/controllers/HelloRestControllerTest.java
+++ spring-boot-hello/src/test/java/com/practice/controllers/HelloRestControllerTest.java
@@ -4,14 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloRestControllerTest {
 
     @Test
+    public void autowiringWorked(@LocalServerPort int port) {
+        assertNotNull(port);
+        System.out.println("Port: " + port);
+    }
+
+    @Test
     public void testHello(@Autowired TestRestTemplate webTestClient) {
-        String body = webTestClient.getForObject("/greet?name={name}", String.class, "Kishore");
-        assertEquals("{\"message\":\"Hello, Kishore!\"}", body);
+        var greeting = webTestClient.getForObject("/greet?name={name}", Greeting.class, "Kishore");
+        assertEquals("Hello, Kishore!", greeting.message());
     }
 }
